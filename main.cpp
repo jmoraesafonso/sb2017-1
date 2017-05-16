1c29cc1ee7d6f497d1fae36d136b5c24dc6833fe
Joao <jmoraesafonso@gmail.com>
2017-05-15 22:54:48 -0300

    Diretiva de processamento

--- main.cpp
+++ main.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
-
+#include <fstream>
+using namespace std; //desambiguacao dos nomes das funcoes
 
 int main (int argc, char *argv[] ){
-
-
-
-
-
-return (0);
+	
+	if (argc != 3){ 
+		cout<<"Erro: Argumento faltante."<<endl;
+		return (0);
+	}
+	
+	else if (argc == 3){
+		ifstream arquivo (argv[2], ifstream::in);
+		if (!arquivo) { cout<<"Erro: impossivell abrir o arquivo."<<endl;
+			return(0);
+		}
+	}
+	
+	 
+	
+	return (0);
 }
